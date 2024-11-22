b90d14a401886e24a2caf0cac55c318673abbc49
Rishat <Rshiriev@it-one.ru>
2024-11-22 20:33:26 +0500

    tmp1

---
+++ src/main/java/org/example/helpers/add.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Вызвали скрипт добавления новой записи! Файл $1"
+
+data=""
+
+echo "Введите фамилию:"
+read surname
+echo "Введите имя:"
+read name
+echo "Введите телефон:"
+read phoneNumber
+echo "Введите заметку:"
+read description
+
+echo "$surname|$name|$phoneNumber|$description" >> $1
--- src/main/java/org/example/phone_book.sh
+++ src/main/java/org/example/phone_book.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-fileName
+
+fileName="tmp.txt"
 if [ "$#" -lt 1 ]; then
-  echo> tmp.txt
+  echo> $fileName
 else
   echo> $1
+  fileName=$1
 fi
 
 PS3="Телефонная книга. Выберите действие:"
+
 select item in add delete edit list_all search_by_name; do
     case $item in
     add)
       echo "Выбрали добавление новой записи."
+      . ./helpers/add.sh $fileName
       ;;
     delete)
       echo "Выбрали удаление записи."
