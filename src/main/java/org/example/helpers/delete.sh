e64c80b786647dace84e744d442541bd971e7770
Rishat <Rshiriev@it-one.ru>
2024-11-23 22:30:23 +0500

    tmp5

--- src/main/java/org/example/helpers/delete.sh
+++ src/main/java/org/example/helpers/delete.sh
@@ -3,12 +3,9 @@ fileName=$1
 
 . ./helpers/search.sh $fileName
 
-echo ${found[2]}
-
 read -p "Уточните запись для удаления:" deleteIndex
 
-stringToDelete=${found[$deleteIndex]}
+stringToDelete=${myArr[$deleteIndex]}
 
 echo "Будет удалена запись:"
-echo ${found[1]}
 echo $stringToDelete
--- src/main/java/org/example/helpers/search.sh
+++ src/main/java/org/example/helpers/search.sh
@@ -3,17 +3,12 @@ fileName=$1
 
 read -p "Введите фамилию и/или имя:" searchText
 
-oldIFS=$IFS
-IFS=$'\n'
 found=$(awk -v pat="$searchText" -F "|" '$1 ~ pat {print $0}' $fileName)
 
 echo "В телефонной книге $fileName найдены следующие записи:"
 
-index=0
+mapfile -t myArr <<< "${found}"
 
-for i in ${found[@]}; do
-    index=$(($index + 1))
-    printf '%d) %s \n' $index $i
+for i in "${!myArr[@]}"; do
+  printf "%d)\t%s\n" "$i" "${myArr[$i]}"
 done
-
-#IFS=$oldIFS
