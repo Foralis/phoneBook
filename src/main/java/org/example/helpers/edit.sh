a5d5a22b407cdafa782a58e566ec3b4d96e331af
Rishat <Rshiriev@it-one.ru>
2024-11-24 20:38:18 +0500

    tmp13

--- src/main/java/org/example/helpers/edit.sh
+++ src/main/java/org/example/helpers/edit.sh
@@ -16,12 +16,14 @@ oldName=$(echo $nameAndSurname | awk '{print $2}')
 oldPhone=$(echo $stringToEdit | awk -F "|" '{print $2}')
 oldDescription=$(echo $stringToEdit | awk -F "|" '{print $3}')
 
-
 read -e -p "Введите новую фамилию:" -i "$oldSurname" newSurname
 read -e -p "Введите новое имя:" -i "$oldName" newName
 read -e -p "Введите новый телефон:" -i "$oldPhone" newPhone
 read -e -p "Введите новую заметку:" -i "$oldDescription" newDescription
 
-newString=$(echo "$newSurname $newName|$newPhone|$newName")
+newString=$(echo "$newSurname $newName|$newPhone|$newDescription")
 
 echo "Новая строка будет выглядет так: " $newString
+
+sed -i "0,/^$stringToEdit\$/{/^$stringToEdit\$/d;}" $fileName
+echo "$newString" >> $fileName
