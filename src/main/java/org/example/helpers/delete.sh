f4b38d42a211798d4bf96a4ae471c55d0ddccd44
Rishat <Rshiriev@it-one.ru>
2024-11-24 13:50:55 +0500

    tmp6

--- src/main/java/org/example/helpers/delete.sh
+++ src/main/java/org/example/helpers/delete.sh
@@ -8,4 +8,17 @@ read -p "Уточните запись для удаления:" deleteIndex
 stringToDelete=${myArr[$deleteIndex]}
 
 echo "Будет удалена запись:"
+
 echo $stringToDelete
+
+pat="^"
+pat+="$stringToDelete"
+pat+="\$"
+
+echo "Такой паттерн:"
+echo $pat
+
+sed -i "0,/$pat/d" $fileName
+
+#awk '!/pattern/' file > temp && mv temp file
+#sed '0,/tat/{/tat/d;}' inputfile
