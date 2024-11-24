ac585b3e83e299b2e5d4a090608289972ef81396
Rishat <Rshiriev@it-one.ru>
2024-11-24 20:15:35 +0500

    tmp12

--- src/main/java/org/example/helpers/edit.sh
+++ src/main/java/org/example/helpers/edit.sh
@@ -11,16 +11,17 @@ echo "Будет отредактирована запись:"
 echo $stringToEdit
 
 nameAndSurname=$(echo $stringToEdit | awk -F "|" '{print $1}')
-oldName=$(echo $nameAndSurname | awk '{print $1}')
-oldSurname=$(echo $nameAndSurname | awk '{print $2}')
+oldSurname=$(echo $nameAndSurname | awk '{print $1}')
+oldName=$(echo $nameAndSurname | awk '{print $2}')
 oldPhone=$(echo $stringToEdit | awk -F "|" '{print $2}')
 oldDescription=$(echo $stringToEdit | awk -F "|" '{print $3}')
-#read -p "Введите новую фамилию :" surname
-#read -p "Введите имя:" name
-#read -p "Введите телефон:" phoneNumber
-#read -p "Введите заметку:" description
 
-read -e -p "Введите новую фамилию:" -i $oldName newName
-read -e -p "Введите новое имя:" -i $oldSurname newSurname
-read -e -p "Введите новый телефон:" -i $oldPhone newPhone
-read -e -p "Введите новую заметку:" -i $oldDescription newDescription
+
+read -e -p "Введите новую фамилию:" -i "$oldSurname" newSurname
+read -e -p "Введите новое имя:" -i "$oldName" newName
+read -e -p "Введите новый телефон:" -i "$oldPhone" newPhone
+read -e -p "Введите новую заметку:" -i "$oldDescription" newDescription
+
+newString=$(echo "$newSurname $newName|$newPhone|$newName")
+
+echo "Новая строка будет выглядет так: " $newString
